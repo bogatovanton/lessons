c1fa632ccd43a1d15135ec1788b561c592e6034e
bogatovanton <86074918+bogatovanton@users.noreply.github.com>
2026-05-06 17:09:02 +0300

    шаблоны django

--- django/firstproject/game/views.py
+++ django/firstproject/game/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse(f"<h1>Hello World</h1>")
+    events = [
+        {"id": 1, "title": "Python Conf 2026", "date": "2026-06-12", "location": "Moscow"},
+        {"id": 2, "title": "Мастер-класс по Django", "date": "2026-05-28", "location": "Nizhny Novgorod"},
+        {"id": 3, "title": "Backend Meetup", "date": "2026-07-04", "location": "St-Petersburg"},
+    ]
+    return render(request, 'form.html')
 
 def profile(request, eq):
     s_id = 0
