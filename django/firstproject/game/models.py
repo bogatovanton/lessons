2ac9153e32419420296da5cee352e845fb0308cd
bogatovanton <86074918+bogatovanton@users.noreply.github.com>
2026-05-12 17:26:56 +0300

    Модели

--- django/firstproject/game/models.py
+++ django/firstproject/game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    id = models.IntegerField(verbose_name="ID", unique=True, primary_key=True)
+    title = models.CharField(verbose_name="Название", max_length=50)
+    date = models.DateField(verbose_name="Дата")
+    location = models.CharField(verbose_name="Локация", max_length=50)
--- django/firstproject/game/views.py
+++ django/firstproject/game/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from game.models import Events
 # Create your views here.
 
 def home(request):
-    events = [
+    
+    #Events.objects.create(title="Python Conf 2026", date="2026-06-12") Создает запись
+    events = Events.objects.all() # Возвращает список объектов строк
+    #Events.objects.get(id=1) Получить одну строку
+    #Events.filter() Выводит строки по условию
+    #event.title = "Мастер-класс по Django" Меняет запись
+    #event.save() Сохраняет изменения в бд
+    #event.delete() удаляет запись
+    events1 = [
         {"id": 1, "title": "Python Conf 2026", "date": "2026-06-12", "location": "Moscow"},
         {"id": 2, "title": "Мастер-класс по Django", "date": "2026-05-28", "location": "Nizhny Novgorod"},
         {"id": 3, "title": "Backend Meetup", "date": "2026-07-04", "location": "St-Petersburg"},
     ]
-    return render(request, 'form.html')
+    return render(request, 'form.html', {"events": events})
 
 def profile(request, eq):
     s_id = 0
