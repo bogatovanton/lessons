ef4ff8ef31ae959dd0da1805d8cbc02e7e02cd89
bogatovanton <86074918+bogatovanton@users.noreply.github.com>
2026-05-20 18:53:22 +0300

    Админка

--- django/firstproject/game/admin.py
+++ django/firstproject/game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Events
 
 # Register your models here.
+
+@admin.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location')
+    search_fields = ('title',)
