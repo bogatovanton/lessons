99f7ad4b538273f3da2989f31102dc097fece9a5
bogatovanton <86074918+bogatovanton@users.noreply.github.com>
2026-05-22 16:54:38 +0300

    Авторизация

--- django/firstproject/firstproject/urls.py
+++ django/firstproject/firstproject/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('equation/<str:eq>', views.profile)
+    path('home/', views.home, name='home'),
+    path('equation/<str:eq>', views.profile),
+    path('register/', views.registration, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+
 ]
--- django/firstproject/game/views.py
+++ django/firstproject/game/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
 from game.models import Events
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def home(request):
@@ -13,12 +16,15 @@ def home(request):
     #event.title = "Мастер-класс по Django" Меняет запись
     #event.save() Сохраняет изменения в бд
     #event.delete() удаляет запись
-    events1 = [
-        {"id": 1, "title": "Python Conf 2026", "date": "2026-06-12", "location": "Moscow"},
-        {"id": 2, "title": "Мастер-класс по Django", "date": "2026-05-28", "location": "Nizhny Novgorod"},
-        {"id": 3, "title": "Backend Meetup", "date": "2026-07-04", "location": "St-Petersburg"},
-    ]
-    return render(request, 'form.html', {"events": events})
+    # events1 = [
+    #     {"id": 1, "title": "Python Conf 2026", "date": "2026-06-12", "location": "Moscow"},
+    #     {"id": 2, "title": "Мастер-класс по Django", "date": "2026-05-28", "location": "Nizhny Novgorod"},
+    #     {"id": 3, "title": "Backend Meetup", "date": "2026-07-04", "location": "St-Petersburg"},
+    # ]
+    # return render(request, 'form.html', {"events": events})
+    user = request.user
+    first_name = user.first_name
+    return render(request, 'form.html', {'name': first_name, 'events': events})
 
 def profile(request, eq):
     s_id = 0
@@ -41,3 +47,26 @@ def profile(request, eq):
         return HttpResponse(f"{eq}={a / b}")
     else:
         return HttpResponse(f"Некорректный ввод")
+    
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'register.html', {'form': form.as_p})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'registration/login.html', {'form': form})
